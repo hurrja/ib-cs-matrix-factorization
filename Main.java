e4c1c012075891d3db04d0fc2ae7b6605b9be5e5
Jarmo Hurri <jarmo.hurri@iki.fi>
2022-09-06 12:31:48 +0300

    * correct partial derivatives * more comments * removal of unnecessary pred value matrix

--- Main.java
+++ Main.java
@@ -9,8 +9,11 @@ public class Main
     final double GAMMA = 0.001; // sgd step size
     final double LAMBDA = 0.01; // regularization weight for user weights
     final double MU = LAMBDA; // regularization weight for degrees
-    final int NUM_SGD_ROUNDS = 1000; // termination condition
 
+    // termination condition: this many rounds over entire data
+    final int NUM_SGD_ROUNDS = 1000; 
+    
+    // observed values
     final int[][] R = {{3, 2, 3, -1, -1},
                        {-1, 2, 2, 2, -1},
                        {3, 4, -1, 4, 2},
@@ -20,53 +23,54 @@ public class Main
     final int NUM_USERS = R.length;
     final int NUM_ITEMS = R [0].length;
 
-    double[][] W = new double [NUM_USERS][F];
+    // factor matrices
+    double[][] W = new double [NUM_USERS][F]; // users to features
     initializeOnes (W);
-    double[][] D = new double [F][NUM_ITEMS];
+    double[][] D = new double [F][NUM_ITEMS]; // features to items
     initializeOnes (D);
 
+    // loop NUM_SGD_ROUNDS over entire data
     for (int r = 0; r < NUM_SGD_ROUNDS; r++)
     {
-      if (r % (NUM_SGD_ROUNDS / 10) == 0) // log 10 times
+      if (r % (NUM_SGD_ROUNDS / 10) == 0) // log progress 10 times
       {
-        // log progress
-        double g = 0; // objective function
+        double g = 0; // value of objective function
         for (int i = 0; i < NUM_USERS; i++)
           for (int j = 0; j < NUM_ITEMS; j++)
           {
             if (R [i][j] >= 0)
             {
-              double d = R [i][j] - predicted (i, j, W, D);
+              double d = R [i][j] - predicted (i, j, W, D); // prediction error
               g += d * d;
             }
           }
           
+        // L2 norms of matrices
         g += LAMBDA * l2Norm (W);
         g += MU * l2Norm (D);
           
         System.out.println ("objective: " + String.format ("%.3g", g));
       }
         
+      // modify factor matrices for all nonnegative observed values
       for (int i = 0; i < NUM_USERS; i++)
         for (int j = 0; j < NUM_ITEMS; j++)
           if (R [i][j] >= 0)
           {
-            double p = predicted (i, j, W, D); // predicted value
-            double e = R [i][j] - p; // prediction error
+            double e = R [i][j] - predicted (i, j, W, D); // prediction error
             
             for (int k = 0; k < F; k++)
             {
               double dw = 2 * (-D [k][j] * e + LAMBDA * W [i][k]); // dg/dw
-              W [i][k] -= GAMMA * dw;
+              W [i][k] -= GAMMA * dw; // gradient descent
               
-              double dd = 2 * (-D [k][j] * e + LAMBDA * W [i][k]); // dg/dd
+              double dd = 2 * (-W [i][k] * e + MU * D [k][j]); // dg/dd
               D [k][j] -= GAMMA * dd;
             }
           }
     }
     
-    double[][] P = new double [NUM_USERS][NUM_ITEMS];
-    
+    // output predicted values
     for (int i = 0; i < NUM_USERS; i++)
     {
       for (int j = 0; j < NUM_ITEMS; j++)
